14259e0b8b5db127e92caf8ff238769b347a5d00
SangwonHan <han19931213@naver.com>
2021-03-17 14:27:31 +0900

    Jenkins Test

--- demo/src/main/java/com/coupon/WebConfig.java
+++ demo/src/main/java/com/coupon/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     /**
+     * test
      * 개발 환경에서의 크로스 도메인 이슈를 해결하기 위한 코드로
      * 운영 환경에 배포할 경우에는 15~18행을 주석 처리합니다.
      * 
